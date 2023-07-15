8311568352f2d5fc91583da025b8575cd11deaae
emma-ajay <emmanuelajayi205@gmail.com>
2023-07-15 19:34:11 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='h-10 w-50 bg-indigo-600 rounded-full text-gray-900'>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
