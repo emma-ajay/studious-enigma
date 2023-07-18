708b55160084e4683800e8e76b38612e343528de
Nottidge <nottidge@Nottidges-MacBook-Pro-2.local>
2023-07-18 13:12:35 +0100

    upload image trial

---
+++ src/components/uploadImage.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FileUploader } from "react-drag-drop-files";
+
+const fileTypes = ["JPG", "PNG", "GIF", "JPEG", "TIF"];
+
+export const uploadImage = () => {
+  return (
+    <div>
+      <FileUploader
+        multiple={false}
+        handleChange={handleUploadChange}
+        name="image"
+        types={fileTypes}
+      >
+        <button className="bg-[#3838F3] hover:bg-[#000088] py-7 px-10 mb-8 flex items-center justify-center text-3xl font-bold text-white rounded-[20px] w-full">
+          <svg
+            className="me-4"
+            width="50"
+            height="47"
+            viewBox="0 0 50 47"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M21.875 25.125H28.125V12.625H37.5L25 0.125L12.5 12.625H21.875V25.125ZM31.25 18.0938V22.9125L45.5594 28.25L25 35.9156L4.44063 28.25L18.75 22.9125V18.0938L0 25.125V37.625L25 47L50 37.625V25.125L31.25 18.0938Z"
+              fill="white"
+            />
+          </svg>
+          Upload Image
+        </button>
+      </FileUploader>
+    </div>
+  );
+};
