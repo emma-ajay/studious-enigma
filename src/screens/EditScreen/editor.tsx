17dcbb88e724373e67a34cf107d2eb29ea76176d
nottidge <freshice57@gmail.com>
2023-07-25 22:56:56 +0100

    custom upload to wang

--- src/screens/EditScreen/editor.tsx
+++ src/screens/EditScreen/editor.tsx
@@ -11,8 +11,11 @@ import { API } from "../../controllers/API";
 import axios from "axios";
 import { DomEditor } from "@wangeditor/editor";
 import { i18nChangeLanguage } from "@wangeditor/editor";
+import { UploadAPI } from "../../controllers/API";
 
 // Switch language - 'en' or 'zh-CN'
+
+type InsertFnType = (url: string, alt: string, href: string) => void;
 
 function MyEditor() {
   const { draftId } = useParams();
@@ -63,14 +66,28 @@ function MyEditor() {
   };
   //@ts-ignore
   editorConfig.MENU_CONF["uploadImage"] = {
-    server: "https://vestri.serveo.net/api/v1/image",
-    headers: {
-      "Content-Type": "multipart/form-data",
-      Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjg5NjgyODEwLCJleHAiOjE2OTAyODc2MTB9.60PdUb82C0r3IrEiG2sYhRbKhN2o_ajrQsjZ23bhAKX_cvA0fLGkV6F5oj7ehcE8O4gC-VUtkGzj_lscFOboCw`,
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, OPTIONS, POST, PUT",
+    async customUpload(file: File, insertFn: InsertFnType) {
+      // TS syntax
+
+      // `file` is your selected file
+      const formData = new FormData();
+      formData.append("image", file);
+      // upload images yourself, and get image's url, alt, href
+      const response = await UploadAPI.post("/api/v1/image", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjg5NjgyODEwLCJleHAiOjE2OTAyODc2MTB9.60PdUb82C0r3IrEiG2sYhRbKhN2o_ajrQsjZ23bhAKX_cvA0fLGkV6F5oj7ehcE8O4gC-VUtkGzj_lscFOboCw`,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, OPTIONS, POST, PUT",
+        },
+      });
+      // insert image
+      insertFn(
+        response.data.object.imageUrl,
+        "an image",
+        response.data.object.imageUrl
+      );
     },
-    fieldName: "custom-field-name",
     // other config...
   };
 
